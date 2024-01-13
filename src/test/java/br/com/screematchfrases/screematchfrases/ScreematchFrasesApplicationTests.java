6563092d696be18647a29da2ae8d7e05f23242f6
matheus mozart <mmsnborges@gmail.com>
2024-01-13 18:26:40 -0400

    first commit

---
+++ src/test/java/br/com/screematchfrases/screematchfrases/ScreematchFrasesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.screematchfrases.screematchfrases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreematchFrasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
